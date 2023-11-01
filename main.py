f82b3d179ed24c5d7857344ec53d973fed389549
sultanbekovr <r1sultanbekov@gmail.com>
2023-11-01 06:05:38 +0600

    add .gitignore change poll to buttons

--- main.py
+++ main.py
@@ -1,64 +1,124 @@
 import asyncio
 import logging
 import os
+import re
 import sys
 from datetime import datetime, timedelta
 
-from aiogram import Bot, Dispatcher, types
+import redis
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
-from aiogram.types import PollAnswer, ChatPermissions
+from aiogram.types import ChatPermissions, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from dotenv import load_dotenv
 
-# Bot token can be obtained via https://t.me/BotFather
-TOKEN = os.getenv('BOT_TOKEN')
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
-# All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
-# Initialize Bot instance with a default parse mode which will be passed to all API calls
 bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+
+
+async def button_create(button_num1, button_num2):
+    builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(
+        text=f"Yes ({button_num1})",
+        callback_data="Yes")
+    )
+    builder.add(InlineKeyboardButton(
+        text=f"No ({button_num2})",
+        callback_data="No")
+    )
+
+    return builder
+
+
+async def get_counter_from_text(text) -> int:
+    list_of_digits = re.findall(r'\d+', text)
+    counter = int(list_of_digits[0])
+
+    return counter
+
+
+async def mute_user(chat_id, user):
+    permissions = ChatPermissions(can_send_messages=False,
+                                  can_send_audios=False,
+                                  can_send_documents=False,
+                                  can_send_photos=False,
+                                  can_send_videos=False,
+                                  can_send_video_notes=False,
+                                  can_send_voice_notes=False,
+                                  can_send_polls=False,
+                                  can_send_other_messages=False,)
+
+    await bot.restrict_chat_member(chat_id=chat_id,
+                                   user_id=user.id,
+                                   permissions=permissions,
+                                   until_date=datetime.now() + timedelta(days=7))
+    await bot.send_message(chat_id=chat_id,
+                           text=f'Пользователь с айди {user.first_name} получт мут на 7 дней')
+
+
+async def check_new_voice(user, message_id) -> bool:
+    host = os.getenv('REDIS_HOST')
+    port = os.getenv('REDIS_PORT')
+    r = redis.Redis(host=host, port=port, decode_responses=True)
+    if r.get(user.first_name + user.last_name + str(message_id)) is not None:
+        return False
+    r.set(user.first_name + user.last_name + str(message_id), 'true', ex=600)
+    return True
 
 
 @dp.message(Command('mute'))
 async def echo_handler(message: types.Message) -> None:
-    user_id = message.from_user
-    if user_id is not None:
-        await message.answer_poll(question=f'Дать муть на 7 дней для {message.reply_to_message.from_user.first_name} ({message.reply_to_message.from_user.id}) ? (Нужно 10 голосов)',
-                                  options=['Yes', 'No'],
-                                  reply_to_message_id=message.reply_to_message.message_id,
-                                  open_period=600)
+    builder = await button_create(0, 0)
+
+    await message.answer(f'Дать муть на 7 дней для {message.reply_to_message.from_user.first_name}?', reply_markup=builder.as_markup(), reply_to_message_id=message.reply_to_message.message_id)
 
 
-@dp.poll()
-async def poll_handler(poll: PollAnswer):
-    text = poll.question
-    user_id = text.split()[-5].replace('(', '').replace(')', '')
-    if poll.options[0].voter_count == 10:
-        permissions = ChatPermissions(can_send_messages=False,
-                                      can_send_audios=False,
-                                      can_send_documents=False,
-                                      can_send_photos=False,
-                                      can_send_videos=False,
-                                      can_send_video_notes=False,
-                                      can_send_voice_notes=False,
-                                      can_send_polls=False,
-                                      can_send_other_messages=False,)
+@dp.callback_query(F.data == 'Yes')
+async def change_yes_button_data(callback: types.CallbackQuery):
+    no_counter = await get_counter_from_text(callback.message.reply_markup.inline_keyboard[0][1].text)
+    yes_counter = await get_counter_from_text(callback.message.reply_markup.inline_keyboard[0][0].text)
 
-        await bot.restrict_chat_member(chat_id='-1001592712917',
-                                       user_id=user_id,
-                                       permissions=permissions,
-                                       until_date=datetime.now() + timedelta(days=7))
+    new_voice = await check_new_voice(callback.from_user, callback.message.message_id)
+    if new_voice:
+        yes_counter += 1
+        builder = await button_create(yes_counter, no_counter)
+        await callback.message.edit_reply_markup(reply_markup=builder.as_markup())
+    else:
+        await bot.answer_callback_query(callback_query_id=callback.id, text='Вы уже проголосовали')
 
-        await bot.send_message(chat_id='-1001592712917',
-                               text=f'Пользователь с айди {user_id} получт мут на 7 дней')
+    if yes_counter >= 10:
+        await bot.delete_message(chat_id=callback.message.chat.id, message_id=callback.message.message_id)
+        await mute_user(callback.message.chat.id, callback.from_user.id)
+
+
+@dp.callback_query(F.data == 'No')
+async def change_no_button_data(callback: types.CallbackQuery):
+    no_counter = await get_counter_from_text(callback.message.reply_markup.inline_keyboard[0][1].text)
+    yes_counter = await get_counter_from_text(callback.message.reply_markup.inline_keyboard[0][0].text)
+
+    new_voice = await check_new_voice(callback.from_user, callback.message.message_id)
+    if new_voice:
+        no_counter += 1
+        builder = await button_create(yes_counter, no_counter)
+        await callback.message.edit_reply_markup(reply_markup=builder.as_markup())
+    else:
+        await bot.answer_callback_query(callback_query_id=callback.id, text='Вы уже проголосовали')
+
+    if no_counter >= 10:
+        await bot.delete_message(chat_id=callback.message.chat.id, message_id=callback.message.message_id)
 
 
 async def main() -> None:
-    # And the run events dispatching
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
+
 
